fff6e0466c87c6fd55061d29be9024780413960c
ffsclyh <44584613+ffsclyh@users.noreply.github.com>
2018-10-30 17:44:07 -0400

    Update services.js

--- ecs/scripts/services.js
+++ ecs/scripts/services.js
@@ -83,7 +83,7 @@ return ensureAuthenticated()
     return group.TargetGroupName.includes(SHORT_GIT_HASH);
   });
   for (const group of groups) {
-    console.log('TargetGroup in LB: " + group.TargetGroupName ) ;  
+    console.log('TargetGroup in LB: %s ' , group.TargetGroupName ) ;  
     if (group.TargetGroupName.includes('users')) {
       USERS_TARGET_GROUP_ARN = group.TargetGroupArn;
     }
